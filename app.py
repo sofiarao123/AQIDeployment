6145ff6313ad3442d90af40c3dce6e4260adc442
sofiarao123 <62967746+sofiarao123@users.noreply.github.com>
2020-03-31 23:27:31 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 21:41:41 2020
+
+@author: MSN_ADMIN
+"""
+
+from flask import Flask,render_template,url_for,request
+import pandas as pd 
+
+import pickle
+
+# load the model from disk
+loaded_model=pickle.load(open('randomForest_model.pkl', 'rb'))
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    df=pd.read_csv('real_2018.csv')
+    my_prediction=loaded_model.predict(df.iloc[:,:-1].values)
+    my_prediction=my_prediction.tolist()
+    return render_template('result.html',prediction = my_prediction)
+
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
